imdark/Heidisql

commit baae822cd7e55b7fa39efa1bf9fcc81f1e040652

Adjust offset stuff in SQL splitter routine, DELIMITER department. Fixes issue #2053.
git-svn-id: http://heidisql.googlecode.com/svn/trunk@3432 560a3889-bb33-0410-9f33-43a8a102dc09

source/helpers.pas
@@ -379,9 +379,9 @@ begin
   DelimLen := Length(Delim);
   Result := TSQLBatch.Create;
   rx := TRegExpr.Create;
-  rx.Expression := '^\s*DELIMITER\s+(\S+)\s*$';
+  rx.Expression := '^\s*DELIMITER\s+(\S+)\s*';
   rx.ModifierI := True;
-  rx.ModifierM := True;
+  rx.ModifierM := False;
   while i < AllLen do begin
     Inc(i);
     // Current and next char
@@ -405,10 +405,10 @@ begin
     if (CharInSet(c, NewLines) and (not CharInSet(n, NewLines))) or (i = 1) then begin
       if i > 1 then
         InComment := False;
-      if (not InString) and (not InBigComment) and rx.Exec(copy(SQL, LastLeftOffset, 100)) then begin
+      if (not InString) and (not InBigComment) and rx.Exec(copy(SQL, i, 100)) then begin
         Delim := rx.Match[1];
-        DelimLen := Length(Delim);
-        Inc(i, rx.MatchLen[0]+1);
+        DelimLen := rx.MatchLen[1];
+        Inc(i, rx.MatchLen[0]);
         LastLeftOffset := i;
         continue;
       end;

commit 16c253f38a2bae0ed80451e754c0452abcf02148

Fix query splitter: "DELIMITER xyz" must be followed by some space char, this is not optionally. Broken in r3432. Fixes issue #2092.

source/helpers.pas
@@ -364,7 +364,7 @@ begin
   DelimLen := Length(Delim);
   Result := TSQLBatch.Create;
   rx := TRegExpr.Create;
-  rx.Expression := '^\s*DELIMITER\s+(\S+)\s*';
+  rx.Expression := '^\s*DELIMITER\s+(\S+)\s';
   rx.ModifierI := True;
   rx.ModifierM := False;
   while i < AllLen do begin